a8bbe7490b835e6262d06c906794b2fcc29d9f73
Kawann <kawannmarcelo7@gmail.com>
2025-06-28 10:15:53 -0300

    feat: detalhes para implementação da ia do gemini

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import routes from './routes/index.js';  // importa as rotas organizadas
+import cors from 'cors';
+import chat from './routes/chat.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
-// usa as rotas no caminho raiz '/'
-app.use('/', routes);
+app.use('/chat', chat);
 
 app.listen(PORT, () => {
     console.log(`API funcionando na porta ${PORT}!`);
